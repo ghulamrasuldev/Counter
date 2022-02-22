d0bacaabd14ded07e7c0b21c85cb35b49ca61323
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2022-02-22 20:07:11 +0500

    Initial Commit

---
+++ Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by GAMA  on 22/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by GAMA  on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
